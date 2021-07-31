7053a716bc50fab96cc68a4c8966d56c4f1fa17d
Nika Medić <nikamedic14@gmail.com>
2021-07-31 15:40:53 +0200

    Scope testing - protoype

--- src/main/java/com/agency04/sbss/pizza/PizzaApp.java
+++ src/main/java/com/agency04/sbss/pizza/PizzaApp.java
@@ -11,15 +11,23 @@ public class PizzaApp {
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
         PizzaDeliveryService pizzaDeliveryService  = context.getBean("pizzaDeliveryService",PizzaDeliveryService.class);
-        Pizza pizza = context.getBean("quatroFormaggi",Pizza.class);
+        //Pizza pizza = context.getBean("quatroFormaggi",Pizza.class);
 
+        PizzaDeliveryService thePizzaDeliveryService = context.getBean("pizzaDeliveryService",PizzaDeliveryService.class);
+        boolean result = (pizzaDeliveryService == thePizzaDeliveryService);
+
+        //If the scope of the bean is singleton (as it is default) then the location should be the same,
+        //because it's the same object, and if the scope is prototype then the location is different
+        //and the @PreDestroy method will not be called.
+        System.out.println("The beans are pointing to the same location: "+result);
+/*
         System.out.println(pizzaDeliveryService.orderPizza(pizza));
 
         PizzeriaService pizzeriaService = context.getBean("dominos",PizzeriaService.class);
         PizzeriaService pizzeriaService2 = context.getBean("pizzeriaLastrada",PizzeriaService.class);
 
         System.out.println(pizzeriaService.getAddress());
-        System.out.println(pizzeriaService2.getName());
+        System.out.println(pizzeriaService2.getName());*/
 
         context.close();
     }
--- src/main/java/com/agency04/sbss/pizza/service/PizzaDeliveryService.java
+++ src/main/java/com/agency04/sbss/pizza/service/PizzaDeliveryService.java
@@ -3,9 +3,14 @@ package com.agency04.sbss.pizza.service;
 import com.agency04.sbss.pizza.model.pizzaPojos.Pizza;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+@Scope("prototype")
 public class PizzaDeliveryService {
 
     @Qualifier("dominos")
@@ -16,5 +21,17 @@ public class PizzaDeliveryService {
         pizzeriaService.makePizza(pizza);
         Thread.sleep(2000);
         return pizza.getName() +" is on it's way :) !";
+    }
+
+    //define my init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("Inside the init method!");
+    }
+
+    //define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("Inside the destroy method!");
     }
 }
